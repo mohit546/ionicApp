4a316a905208e963ebef677b9bf64187e03658b9
Mohit Patil <mohitpatil546@gmail.com>
2016-03-14 12:17:59 +0530

    createTask modal is modified

--- ionicApp/www/js/app.js
+++ ionicApp/www/js/app.js
@@ -5,25 +5,27 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('ionicApp', ['ionic', 'ionicApp.controllers', 'ionicApp.services'])
+angular.module('ionicApp', ['ionic', 'ionicApp.controllers', 'ionicApp.services', 'LocalStorageModule'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+	// for form inputs)
+	if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+	  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	  cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+	}
+	if (window.StatusBar) {
+	  // org.apache.cordova.statusbar required
+	  StatusBar.styleDefault();
+	}
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
+	localStorageServiceProvider
+		.setPrefix('ionicApp');
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
@@ -32,51 +34,51 @@ angular.module('ionicApp', ['ionic', 'ionicApp.controllers', 'ionicApp.services'
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+	.state('tab', {
+	url: '/tab',
+	abstract: true,
+	templateUrl: 'templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
 
   .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
+	url: '/dash',
+	views: {
+	  'tab-dash': {
+		templateUrl: 'templates/tab-dash.html',
+		controller: 'DashCtrl'
+	  }
+	}
   })
 
   .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+	  url: '/chats',
+	  views: {
+		'tab-chats': {
+		  templateUrl: 'templates/tab-chats.html',
+		  controller: 'ChatsCtrl'
+		}
+	  }
+	})
+	.state('tab.chat-detail', {
+	  url: '/chats/:chatId',
+	  views: {
+		'tab-chats': {
+		  templateUrl: 'templates/chat-detail.html',
+		  controller: 'ChatDetailCtrl'
+		}
+	  }
+	})
 
   .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+	url: '/account',
+	views: {
+	  'tab-account': {
+		templateUrl: 'templates/tab-account.html',
+		controller: 'AccountCtrl'
+	  }
+	}
   });
 
   // if none of the above states are matched, use this as the fallback
--- ionicApp/www/js/controllers.js
+++ ionicApp/www/js/controllers.js
@@ -1,5 +1,50 @@
 angular.module('ionicApp.controllers', [])
 
+.controller('ionicAppCtrl', function($scope, $ionicModal, localStorageService){
+	$scope.tasks = [];
+	$scope.task = {};
+
+	$ionicModal.fromTemplateUrl('new-task-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.newTaskModal = modal;
+    });
+
+	$scope.getTasks = function(){
+		if (localStorageService.get('taskData')) {
+			$scope.tasks = localStorageService.get('taskData');
+		} else {
+			$scope.tasks = [];
+		}
+	};
+	$scope.createTask = function(){
+		$scope.tasks.push($scope.task);
+		localStorageService.set('taskData', $scope.tasks);
+		$scope.task = {};
+		$scope.newTaskModal.hide();
+	};
+	$scope.removeTask = function(index){
+		console.log($scope.tasks);
+		$scope.tasks.splice(index, 1);
+		localStorageService.set('taskData', $scope.tasks);
+	};
+	$scope.completeTask = function(index){
+		if (index !== -1) {
+			$scope.tasks[index].completed = true;
+		}
+		localStorageService.set('taskData', $scope.tasks);
+	};
+
+	$scope.openTaskModal = function () {
+		$scope.newTaskModal.show();
+	};
+
+	$scope.closeTaskModal = function () {
+		$scope.newTaskModal.hide();
+	};
+})
+
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -13,7 +58,7 @@ angular.module('ionicApp.controllers', [])
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
-    Chats.remove(chat);
+	Chats.remove(chat);
   };
 })
 
@@ -23,6 +68,6 @@ angular.module('ionicApp.controllers', [])
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
-    enableFriends: true
+	enableFriends: true
   };
 });
